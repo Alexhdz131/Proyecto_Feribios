d165a94a7331e42b45a5bab6e392a450f26d7297
Alexis Hernandez Perez <1717110754@utectulancingo.edu.mx>
2019-04-04 10:13:26 -0600

    Inicio de sesion funcionando

--- app/src/main/java/com/example/proyecto_feribios/MainActivity.java
+++ app/src/main/java/com/example/proyecto_feribios/MainActivity.java
@@ -6,7 +6,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -18,6 +20,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button boton =(Button) findViewById(R.id.button);
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String usuario = ((EditText)findViewById(R.id.editText)).getText().toString();
+                String password = ((EditText)findViewById(R.id.editText3)).getText().toString();
+                if (usuario.equals("admin")&& password.equals("admin"))
+                {
+                    Intent nuevoform = new Intent(MainActivity.this, Menu
+                            .class);
+                    startActivity(nuevoform);
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(),"Usuario Incorrecto",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
         btn=(ImageButton) findViewById(R.id.imageButton);
         btn.setOnClickListener(this);
 
--- app/src/main/java/com/example/proyecto_feribios/Menu.java
+++ app/src/main/java/com/example/proyecto_feribios/Menu.java
@@ -2,6 +2,10 @@ package com.example.proyecto_feribios;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Menu extends AppCompatActivity {
 
