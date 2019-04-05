7d5c7d19d50b23efc755751d86b297aa025aaaee
Alexis Hernandez Perez <1717110754@utectulancingo.edu.mx>
2019-04-05 10:32:54 -0600

    Actualizacion de interfaces

--- app/src/main/java/com/example/proyecto_feribios/Galeria.java
+++ app/src/main/java/com/example/proyecto_feribios/Galeria.java
@@ -1,7 +1,9 @@
 package com.example.proyecto_feribios;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Galeria extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class Galeria extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_galeria);
+    }
+
+    public void Menu(View view) {
+        Intent menu = new Intent(this, Menu.class);
+        startActivity(menu);
     }
 }
--- app/src/main/java/com/example/proyecto_feribios/Menu.java
+++ app/src/main/java/com/example/proyecto_feribios/Menu.java
@@ -1,5 +1,6 @@
 package com.example.proyecto_feribios;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,5 +14,16 @@ public class Menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+
+    public void Galeria(View view) {
+        Intent galeria = new Intent(this, Galeria.class);
+        startActivity(galeria);
+
+    }
+
+    public void ProximosEventos(View view) {
+        Intent eventos = new Intent(this, Proximoseventos.class);
+        startActivity(eventos);
     }
 }
