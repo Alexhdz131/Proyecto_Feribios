a00a562b593557273e4b0f459fcb7846ac9d0744
Alexis Hernandez Perez <1717110754@utectulancingo.edu.mx>
2019-04-11 10:45:52 -0500

    Se agrego mas informacion

--- app/src/main/java/com/example/proyecto_feribios/Proximoseventos.java
+++ app/src/main/java/com/example/proyecto_feribios/Proximoseventos.java
@@ -21,4 +21,23 @@ public class Proximoseventos extends AppCompatActivity {
         Intent tallerdelectura = new Intent(this, Tallerdelectura.class);
         startActivity(tallerdelectura);
     }
+
+    public void Proyeccion(View view) {
+        Intent proyeccion = new Intent(this, Proyeccion.class);
+        startActivity(proyeccion);
+    }
+    public void Gastronomiaanaya(View view) {
+        Intent gastronimiaanaya = new Intent(this, Gastronomiaanaya.class);
+        startActivity(gastronimiaanaya);
+    }
+    public void Cursodeajedrez(View view) {
+        Intent cursodeajedrez= new Intent(this, Cursodeajedrez.class);
+        startActivity(cursodeajedrez);
+    }
+    public void Ficirco(View view) {
+        Intent ficirco= new Intent(this, Ficirco.class);
+        startActivity(ficirco);
+    }
+
+
 }
