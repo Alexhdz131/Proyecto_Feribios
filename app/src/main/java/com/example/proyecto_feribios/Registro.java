3cfb55ae08d868a177d27054c734a8f0f01280eb
israel merida lopez <1717110625@utectulancingo.edu.mx>
2019-04-15 09:17:49 -0500

    actualizacion

--- app/src/main/java/com/example/proyecto_feribios/Registro.java
+++ app/src/main/java/com/example/proyecto_feribios/Registro.java
@@ -1,7 +1,9 @@
 package com.example.proyecto_feribios;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Registro extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class Registro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+    }
+
+    public void Registro(View view) {
+        Intent Registro = new Intent(this, Menu.class);
+        startActivity(Registro);
     }
 }
